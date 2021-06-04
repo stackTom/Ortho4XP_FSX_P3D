7428300fdace73a74c33f8702f83a25d9e9ca789
stackTom <stackTom@users.noreply.github.com>
2021-06-04 18:32:31 -0400

    fix error message making user think program is frozen when it's not
    don't show "FSX/P3D building requires both the tile and mask zooms to equal..."
    message until we are assured user has a tile selected

--- src/O4_GUI_Utils.py
+++ src/O4_GUI_Utils.py
@@ -1100,6 +1100,9 @@ class Ortho4XP_Earth_Preview(tk.Toplevel):
         return
 
     def batch_build(self):
+        list_lat_lon=sorted(self.dico_tiles_todo.keys())
+        if not list_lat_lon: return
+
         O4_ESP_Globals.build_for_ESP = True if self.v_["Build For ESP (FSX/P3D)"].get() else False
         O4_ESP_Globals.do_build_masks = True if self.v_["Draw water masks"].get() else False
 
@@ -1109,8 +1112,6 @@ class Ortho4XP_Earth_Preview(tk.Toplevel):
                 print("FSX/P3D building requires both the tile and mask zooms to equal. Setting the mask zoom to the tile zoom of " + str(main_window_tile_zoom))
                 CFG.mask_zl = main_window_tile_zoom
 
-        list_lat_lon=sorted(self.dico_tiles_todo.keys())
-        if not list_lat_lon: return
         (lat,lon)= list_lat_lon[0]
         try:
             tile=CFG.Tile(lat,lon,self.custom_build_dir)
